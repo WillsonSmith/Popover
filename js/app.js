f2b0d3b8d914fb25880909d66345afcffb7d534f
Willson Smith <me@willsonsmith.com>
2016-10-23 17:45:10 -0400

    webpack

--- js/app.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-  const popoverElement = document.getElementById('popover-selector');
-  const popover = new Popover(popoverElement);
-
-  const popoverSecondaryElement = document.getElementById('popover-secondary');
-  const popoverSecondary = new Popover(popoverSecondaryElement);
-
-})();
